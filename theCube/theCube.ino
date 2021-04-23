af474baf9983aa876ba6f03373114ab8ef06b6f7
Bronson <bronson.jordan@gpgc.org>
2021-04-23 01:14:21 -0500

    Improved debug functionality of Cube

--- theCube/theCube.ino
+++ theCube/theCube.ino
@@ -1,9 +1,26 @@
+#include <ArduinoBLE.h>
 #include <Arduino_LSM9DS1.h>
 
+#define DEBUG
 
 void setup() {
+
+	#ifdef DEBUG
 	Serial.begin(9600);
+	#endif
+
+	//Initialize Sensors
 	IMU.begin();
+
+	#ifdef DEBUG
+	Serial.println("Sensors initialized");
+	#endif
+
+
+
+	#ifdef DEBUG
+	Serial.println("X\tY\tZ");
+	#endif
 }
 
 void loop() {
@@ -12,12 +29,14 @@ void loop() {
 	if (IMU.gyroscopeAvailable()) {
 		IMU.readGyroscope(x, y, z);
 
-		Serial.print(int(x));
-		Serial.print('\t');
-		Serial.print(int(y));
-		Serial.print('\t');
-		Serial.print(int(z));
-		Serial.println('\t');
+		#ifdef DEBUG
+		Serial.print(x, 0);
+		Serial.print("   \t");
+		Serial.print(y, 0);
+		Serial.print("   \t");
+		Serial.print(y, 0);
+		Serial.print("   \r");
+		#endif
 	}
 	delay(100);
 }
