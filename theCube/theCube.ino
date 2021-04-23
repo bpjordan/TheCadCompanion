4fed219d4901e5ddd491d8696df1085788f6687c
Bronson <bronson.jordan@gpgc.org>
2021-04-23 01:42:15 -0500

    Added BLE Searching to Cube

--- theCube/theCube.ino
+++ theCube/theCube.ino
@@ -1,7 +1,8 @@
 #include <ArduinoBLE.h>
 #include <Arduino_LSM9DS1.h>
 
-#define DEBUG
+//#define DEBUG
+#define BLEDEBUG
 
 void setup() {
 
@@ -20,23 +21,75 @@ void setup() {
 
 	#ifdef DEBUG
 	Serial.println("X\tY\tZ");
+	#else
+
+	//Initialize BLE
+	BLE.begin();
+
+	//Scan for the base station
+	BLE.scanForUuid("c9930826-a3ed-11eb-bcbc-0242ac130002")
+
 	#endif
 }
 
-void loop() {
+void loop(){
+
+	//Skip over all of this if we're just debugging the sensor
+	#ifdef DEBUG
+	printSensors();
+	#else
+
+	BLEDevice baseStation = BLE.available();
+
+	if (baseStation){
+		#ifdef BLEDEBUG
+		Serial.print("Found ");
+		Serial.print(baseStation.address());
+		Serial.print(" '");
+		Serial.print(baseStation.localName());
+		Serial.print(" '");
+		Serial.print(baseStation.advertisedServiceUuid());
+		Serial.println();
+		#endif
+
+		if (baseStation.localName() != "BaseStation")
+			return;
+		
+	}
+
+	//We found the right peripheral, so stop scanning
+	BLE.stopScan();
+
+	//And do the thing
+	runSensors();
+	
+	//When we get out of that function, we've lost BLE
+	//So find it again
+	BLE.scanForUuid("c9930826-a3ed-11eb-bcbc-0242ac130002")
+
+	#endif
+
+}
+
+#ifdef DEBUG
+void printSensors() {
 	float x,y,z;
 
 	if (IMU.gyroscopeAvailable()) {
 		IMU.readGyroscope(x, y, z);
 
-		#ifdef DEBUG
 		Serial.print(x, 0);
 		Serial.print("   \t");
 		Serial.print(y, 0);
 		Serial.print("   \t");
 		Serial.print(y, 0);
 		Serial.print("   \r");
-		#endif
 	}
 	delay(100);
+}
+#endif
+
+
+void runSensors(){
+	
 }
